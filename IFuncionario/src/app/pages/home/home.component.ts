8d8f23d98f567f721e8ab90bd38bc22e79a039cc
FERNANDO ramos <rfernando0513@gmail.com>
2024-03-30 23:29:46 -0300

    start consum api and backend, consum api GetFuncionario, create function ngOnit (pesquise funcionararios database), search( pesquise funcionario database on filter)

--- IFuncionario/src/app/pages/home/home.component.ts
+++ IFuncionario/src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FuncionarioService } from '../../services/funcionario.service';
+import { Funcionario } from '../../models/Funcionarios';
+import { log } from 'console';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,33 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  Funcionarios: Funcionario[] = [];
+  FuncionarioGeral: Funcionario[] = [];
+
+  constructor( private funcionarioService: FuncionarioService){}
+
+  ngOnInit(): void{
+    this.funcionarioService.GetFuncionarios().subscribe(data =>{
+      const dados = data.dados;
+
+      dados.map((item) =>{
+        item.dataCriacao = new Date(item.dataCriacao!).toLocaleDateString('pt-BR')
+        item.dataAlteracao = new Date(item.dataAlteracao!).toLocaleDateString('pt-BR')
+      })
+
+      this.Funcionarios = data.dados;
+      this.FuncionarioGeral = data.dados;
+    });
+  }
+
+  search(event: Event){
+    const target = event.target as HTMLInputElement;
+    const value = target.value.toLowerCase();
+
+    this.Funcionarios = this.FuncionarioGeral.filter(funcionario => {
+      return funcionario.nome.toLowerCase().includes(value);
+    })
+  }
 }
